5116671837068b6df4b8ed53063cbb4c4f7d2ea7
DovahChief <pepelimonta@gmail.com>
2017-04-26 00:28:34 -0500

    terminada lista doble

--- src/Algorithms/DLinkList.java
+++ src/Algorithms/DLinkList.java
@@ -16,7 +16,10 @@ public class DLinkList {
     //inserta al comienzo
     public void ins_primera_pos(String _dat, int _num){
         Dlink nuevo = new Dlink(_dat, _num);
+        
         if(es_vacio()) m_ultimo_link = nuevo;
+        else           m_primer_link.anterior = nuevo;
+        
         nuevo.siguiente = m_primer_link;
         m_primer_link = nuevo;
     }
@@ -24,12 +27,27 @@ public class DLinkList {
     //inserta al final
     public void ins_ultima_pos(String _dat, int _num){
         Dlink nuevo = new Dlink(_dat, _num);
+        
         if(es_vacio()) m_primer_link = nuevo;
-        else m_ultimo_link.siguiente = nuevo;
+        else{
+            m_ultimo_link.siguiente = nuevo;
+            nuevo.anterior = m_ultimo_link;
+        } 
+        
         m_ultimo_link = nuevo;
     }
     
     //revisa si la lista esta vacia
     public boolean es_vacio(){ return (m_primer_link == null);}
+    
+    public void display(){
+        Dlink n = m_primer_link;
+        while(n != null){
+            n.display();
+            System.out.println("Siguiente -> " + n.siguiente);
+            n = n.siguiente;
+            System.out.println();
+        }
+    }
     
 }
